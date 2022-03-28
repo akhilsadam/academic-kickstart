6cc31b74f3f6156142c80143861c6b6c1ed5d4e7
Akhil Sadam <sadam.akhil@gmail.com>
2022-03-27 19:29:13 -0500

    [auto] [glob] update

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,44 +1,39 @@
-alert("CustomJS");
+function resetHeight(){
 
-const scrollY = document.body.style.top;
-document.body.style.position = '';
-document.body.style.top = '';
-window.scrollTo(0, parseInt(scrollY || '0') * -1);
+let article = document.querySelector('.inner');
+//document.querySelector('#top');
+let main = document.getElementById('main');
 
+//if(main){
+//alert("main");
+//}else{
+//alert("main FAILED");
+//}
 
-//let element = document.createElement("div");
-//element.className = "bkgd"; 
-//element.id = "bkgd"; 
-//let uwp = document.querySelector('.universal-wrapper.pt-3');
-//element.appendChild(document.createTextNode(''));
-//document.getElementById('top').insertBefore(element,uwp);
-
-
-let article = document.querySelector('#top');
-let main = document.querySelector('#main');
-
-if(main){
-alert(main);
-}else{
-alert("mian FAILED");
-}
-
+//var pseudoBeforeHeight = window.getComputedStyle(article, ':before').height;
+//var offHeight = parseInt(pseudoBeforeHeight);
 var offHeight = article.scrollHeight;
 //var vWidth = document.documentElement.clientWidth;
 var verticalHeight = document.documentElement.clientHeight;
 let offsetHeight = offHeight + 0.25*verticalHeight;
 //let offsetWidth = vWidth;
+
+
+
 let offd = offsetHeight + 'px'
 
-if(offsetHeight){
-alert(offsetHeight);
-alert("Height success");
-}else{
-alert("Height FAILED");
-}
+//if(offsetHeight){
+//alert(offsetHeight);
+//alert("Height success");
+//}else{
+//alert("Height FAILED");
+//}
+
 main.style.setProperty("height",offd, "important");
 //document.getElementById('main').style.setProperty("", "red", "important"); = offsetWidth+'px';
 var gHeight = main.offsetHeight;
+
+console.log("Height");
 
 if(gHeight){
 //alert(gHeight);
@@ -46,3 +41,12 @@ if(gHeight){
 }else{
 //alert("Height FAILED");
 }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	resetHeight();
+	document.querySelector(".skill").addEventListener("hover",resetHeight());
+});
